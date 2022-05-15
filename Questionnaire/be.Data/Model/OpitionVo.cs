513703b6ff7373b1574b79f1275dc691d0e3f0bf
CaoThanhLong <thanhlong2803@hotmail.com>
2022-05-15 18:21:03 +0700

    update code

---
+++ Questionnaire/be.Data/Model/OpitionVo.cs
@@ -0,0 +1,9 @@
+﻿namespace be.Data.Model
+{
+    public class OpitionVo
+    {
+        public long OpitionId { get; set; }
+        public long QuestionId { get; set; }
+        public string OpitionName { get; set; }
+    }
+}
---
+++ Questionnaire/be.Data/Model/QuestionVo.cs
@@ -0,0 +1,11 @@
+﻿namespace be.Data.Model
+{
+    public class QuestionVo
+    {
+        public long QuestionId { get; set; }
+        public string QuestionName { get; set; }
+
+        public ICollection<OpitionVo> Opitions { get; set; }
+        public ICollection<Test_Question_Mapping> Test_Question_Mappings { get; set; }
+    }
+}
