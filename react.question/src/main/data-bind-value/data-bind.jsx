ce46e8db5e11ed16f34c9206392a1cd93c34f7f6
CaoThanhLong <thanhlong2803@hotmail.com>
2022-05-31 07:48:04 +0700

    update code

--- react.question/src/main/data-bind-value/data-bind.jsx
+++ react.question/src/main/data-bind-value/data-bind.jsx
@@ -1,21 +1,21 @@
 
 import React, { useState } from "react";
 
+
 export default function BindingDataValue(props) {
     const [model, setModel] = useState('');
 
     const onClick = (e) => {
         console.log(model);
     }
-    
+
     const handleChange = (event) => {
         const value = event?.target?.value;
-        setModel({ ...model, [event.target.name]: value });    
+        setModel({ ...model, [event.target.name]: value });
     };
-
     return (
         <div>
-            <h1>{model.showInfo}</h1>
+            <h1>{model.firstname + model.lastname}</h1>
 
             <input id="firstname" type="text" name="firstname" onChange={handleChange} value={model.firstname} />
             <input id="lastname" type="text" name="lastname" onChange={handleChange} value={model.lastname} />
